a32cfea457963a037d3f84016477eeb6226768fc
Juan Serna <2016200215@untels.edu.pe>
2020-06-30 16:48:57 -0500

    Prog001 = Prog005Listo

---
+++ src/estructuras/Prog005.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructuras;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Eddy
+ */
+public class Prog005 {
+     public static void main(String[] args){
+     Scanner teclado =new Scanner(System.in);
+      int a,b,temp;
+   System.out.print("Valor a= ");
+   a=Integer.valueOf(teclado.nextLine());
+   System.out.print("Valor b= ");
+   b=Integer.valueOf(teclado.nextLine());
+System.out.println("[" + a + "," + b + "]");
+        if (a>b){
+            temp = a;
+            a = b;
+            b = temp;
+        }
+        do{
+            System.out.println(a);
+            a++;
+        }while (a <= b);
+        teclado.close();
+     }
+}
