0f613fc2c6df257295465016de09d20b498c9c8b
Juan Serna <2016200215@untels.edu.pe>
2020-07-07 14:13:50 -0500

    Prog 007- Metodo Vacio

---
+++ src/modularidad/Prog007.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modularidad;
+//Autor: Juan Serna
+public class Prog007 {
+    //Metodo vacio...
+    //Vacio porque demtro del parentesis esta vacio...
+    public static void saludo(){
+        System.out.println("Bienvenido al Sistema");
+    }
+     public static void mensaje(){
+        System.out.println("En todo tiempo ama el amigo...Prov. 17:17");
+    }
+    
+     public static void despedida(){
+        System.out.println("Gracias por usar este programa");
+    }
+    public static void main(String[] args){
+        saludo();
+        mensaje();
+        despedida();
+        
+    }
+    
+}
